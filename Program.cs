a46e8862f2357beb72ff1d049c80e71be5cf36bc
James Tiu <jamestiulawyer@gmail.com>
2021-04-21 12:54:32 -0600

    created a list, added and removed

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 // Let's play 'New Mexico Jeoparday! 
 //Today' category: 'Famous People from New Mexico'! We will call a famous New Mexico Person an 'object'.
@@ -31,18 +32,41 @@ public class OurListFamousNewMexicoPersons
         List<FamousPerson> NMpeople = new List<FamousPerson>();
 
         // Add parts to the list.
+        Console.WriteLine("List of famous New Mexico Persons");
         NMpeople.Add(new FamousPerson() { FamousPersonName = "Carlos Condit(born 1984) – born in Albuquerque, UFC fighter", FamousPersonId = 1 });
         NMpeople.Add(new FamousPerson() { FamousPersonName = "Bob Foster (born 1938) – born in Albuquerque, light-heavyweight boxer, world champion", FamousPersonId = 2 });
         NMpeople.Add(new FamousPerson() { FamousPersonName = "Holly Holm (born 1981) – born in Albuquerque, boxer/mixed martial fighter", FamousPersonId = 3 });
         NMpeople.Add(new FamousPerson() { FamousPersonName = "Diego Sanchez (born 1981) – born in Albuquerque, UFC fighter", FamousPersonId = 4 });
         NMpeople.Add(new FamousPerson() { FamousPersonName = "Neil Patrick Harris (born 1973) – actor, How I Met Your Mother/Doogie Howser, M.D.", FamousPersonId = 5 });
         NMpeople.Add(new FamousPerson() { FamousPersonName = "Andre Roberson (born 1991) – born in Las Cruces, NBA player", FamousPersonId = 6 });
+        NMpeople.Add(new FamousPerson() { FamousPersonName = "Johnny Tapia (1967 -2012 1991) – born in Albuquerque, boxer", FamousPersonId = 7 });
+        NMpeople.Add(new FamousPerson() { FamousPersonName = "Onawa Lynn Lacy (born June 10, 1982) – from Gamerco, Miss New Mexico USA 2006 title", FamousPersonId = 8 });
 
         Console.WriteLine();
 
         foreach (FamousPerson aFamousPerson in NMpeople)
         {
             Console.WriteLine(aFamousPerson);
+        }
+       
+        //insert the remove method
+        // This will remove person 3,
+
+        NMpeople.Remove(new FamousPerson() { FamousPersonId = 3, FamousPersonName = "Holly Holm" });
+        
+        NMpeople.RemoveAt(3);
+
+        Console.WriteLine("\nremove Holly Holm\n");
+        foreach (FamousPerson afamousPerson in NMpeople)
+        {
+            Console.WriteLine(afamousPerson);
+            
+        }
+        Console.WriteLine("");
+        NMpeople.Count();
+        foreach (FamousPerson afamousPerson in NMpeople)
+        {
+            Console.WriteLine(afamousPerson);
         }
     }
 }
